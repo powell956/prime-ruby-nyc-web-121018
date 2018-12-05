485bdf7e624e7091104e3501e49119da8ec4d946
powell956 <powell956@gmail.com>
2018-12-05 09:34:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+# def prime?(num)
+#   if num < 2
+#     return false
+#   end
+#   #counter = 2
+#   for divisor in 2..(num - 1)
+#     if num % counter == 0
+#       return false
+#     end
+#     counter++
+#   end
+#   true
+# end
+##^Why doesn't this work?
+
+def prime?(num)
+  if num < 2
+    return false
+  end
+  for d in 2..(num - 1)
+   if (num % d) == 0
+    return false
+   end
+  end
+  true
+end
